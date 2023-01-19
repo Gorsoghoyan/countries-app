585e09119fa44ba2af4859cea517fb7c1eb87a48
Gorsoghoyan <soghoyangor99@gmail.com>
2023-01-19 19:28:10 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loading from "./components/Loading";
-
+import GoTopArrow from "./components/GoTopArrow";
 import PublicRoute from "./routes/PublicRoute";
 import PrivateRoute from "./routes/PrivateRoute";
 
@@ -14,23 +14,26 @@ const Dashboard = lazy(() => import("./pages/Dashboard"));
 function App() {
   
   return (
-    <Suspense fallback={<Loading />}> 
-    <Routes>
-      <Route path="admin" element={<PrivateRoute />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="countries" element={<Countries />} />
-        <Route path="accounts" element={<Accounts />} />
-      </Route>
+    <>
+      {/* <Suspense fallback={<Loading />}> 
+      <Routes>
+        <Route path="admin" element={<PrivateRoute />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="countries" element={<Countries />} />
+          <Route path="accounts" element={<Accounts />} />
+        </Route>
 
-      <Route path="user" element={<PublicRoute />}>
-        <Route path="login" element={<Auth type="login" />} />
-        <Route path="register" element={<Auth type="register" />} />
-      </Route>
+        <Route path="user" element={<PublicRoute />}>
+          <Route path="login" element={<Auth type="login" />} />
+          <Route path="register" element={<Auth type="register" />} />
+        </Route>
 
-      <Route path="*" element={<Error />} />
-    </Routes>
-    </Suspense>
+        <Route path="*" element={<Error />} />
+      </Routes>
+      </Suspense> */}
+      <GoTopArrow />
+    </>
   );
 }
 
-export default App;
+// export default App;
